e01c3ff817f7e122967546979b2547ee5132945d
George <solokhin-eg@yandex.ru>
2021-02-10 00:20:08 +0300

    .

---
+++ last/src/Models/Model.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MyApp\Models;
+
+use MyApp\App;
+use MyApp\DB;
+
+abstract class Model
+{
+    protected static function db(): DB
+    {
+        print_r(App::getInstance()->getDB());
+        return App::getInstance()->getDB();
+    }
+
+    protected static function link(): \PDO
+    {
+        return App::getInstance()->getDB()->getLink();
+    }
+}
